2221c3e0e2679717e7e55085008802c8ba26f79d
diegortiz122017-svg <diegortiz122017@gmail.com>
2026-06-04 13:58:21 -0400

    Create Constantes.java

---
+++ firmador/src/main/java/sv/mh/fe/constantes/Constantes.java
@@ -0,0 +1,6 @@
+package sv.mh.fe.constantes;
+
+public class Constantes {
+	public static final String DIRECTORY_UPLOADS = System.getProperty("user.dir") + "/uploads";
+	public static final String DIRECTORY_CERTIFICATE = System.getenv("CERTIFICATE_HOME");
+}
